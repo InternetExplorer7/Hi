f259442b9698e57a984192dbe44214ce1d0b3780
InternetExplorer7 <themaster231436@gmail.com>
2014-10-20 21:24:29 -0700

    Updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var nicknames = [];
 
 
 
@@ -10,13 +11,34 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+	var updatenicknames = function(){
+		io.emit('username', nicknames);
+	}
 	console.log('new user connected');
+	socket.on('new user', function(data,callback){
+if(nicknames.indexOf(data) != -1){
+	callback(false);
+} else{
+	callback(true);
+	socket.nickname = data;
+	nicknames.push(socket.nickname);
+	console.log('bout to emit');
+	console.log(socket.nickname);
+	updatenicknames();
+}
+	});
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  	console.log(msg);
+    io.emit('chat message', msg, socket.nickname);
+  });
+  socket.on('disconnect' , function(msg){
+  	console.log(socket.nickname + " Has disconnected");
+  	 if(!socket.nickname) return;
+  	 nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+  	 updatenicknames();
   });
 });
 
 http.listen(process.env.PORT || 5000, function(){
-  console.log('listening');
+  console.log('listening on port' + process.env.PORT);
 });
-
